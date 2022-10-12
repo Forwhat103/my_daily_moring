f13791d6f5e287a40f68346af437fbdd2875c27a
Forwhat <942587133@qq.com>
2022-10-12 15:35:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,15 +20,26 @@ birthday = os.environ['BIRTHDAY']
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
-user_id = os.environ["USER_ID"]
+user_id = [os.environ["USER_ID"]]
 template_id = os.environ["TEMPLATE_ID"]
 
 
+#def get_weather():
+#  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+#  res = requests.get(url).json()
+#  weather = res['data']['list'][0]
+#  return weather['weather'], weather['humidity'], weather['wind'], weather['airData'], weather['airQuality'], math.floor(weather['temp']), math.floor(weather['low']), math.floor(weather['high'])
+
 def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+  url = "http://t.weather.sojson.com/api/weather/city/" + city
   res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], weather['humidity'], weather['wind'], weather['airData'], weather['airQuality'], math.floor(weather['temp']), math.floor(weather['low']), math.floor(weather['high'])
+  citys = res['cityInfo']
+  weather = res['data']['forecast']
+  humidity = res['data']['shidu']
+  air_data = res['data']['aqi']
+  air_quality = res['data']['quality']
+  temperature = res['data']['wendu'] + "℃"
+  return humidity, wind, air_data, air_quality, temperature, weather, citys
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
@@ -51,11 +62,27 @@ def get_random_color():
 
 
 client = WeChatClient(app_id, app_secret)
-
 wm = WeChatMessage(client)
-wea, humidity, wind, air_data, air_quality, temperature, lowest, highest = get_weather()
+
+
+weather_list, city_list = get_weather()
+# 划分天气信息
+print(weather_list)
+weather = weather_list[0]['type']
+highest = weather_list[0]['high']
+lowest = weather_list[0]['low']
+notice = weather_list[0]['notice']
+wind = weather_list[0]['fx'] + " " + weather_list[0]['fl']
+# 划分城市
+parent = city_list['parent']
+citys = city_list['city']
+
+#wea, humidity, wind, air_data, air_quality, temperature, lowest, highest = get_weather()
 data = {"date":{"value":today1, "color":get_random_color()},
         "week_day":{"value":week_day, "color":get_random_color()},
+        "parent":{"value":parent, "color": get_random_color()},
+        "city":{"value":citys, "color": get_random_color()},
+        "notice":{"value":notice, "color": get_random_color()},
         "weather":{"value":wea, "color":get_random_color()},
         "humidity":{"value":humidity, "color":get_random_color()},
         "wind":{"value":wind, "color":get_random_color()},
@@ -67,5 +94,7 @@ data = {"date":{"value":today1, "color":get_random_color()},
         "love_days":{"value":get_count(), "color":get_random_color()},
         "birthday_left":{"value":get_birthday(), "color":get_random_color()},
         "words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(user_id, template_id, data)
-print(res)
+
+for i in range(len(user_id)):
+  res = wm.send_template(user_id[i], template_id, data)
+  print(res)
